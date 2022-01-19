6e63e8bb99ebf832cbff0bdc278d2a83fb08d8de
LuizFelipe13 <luiz_felipesbc@hotmail.com>
2022-01-19 17:55:45 -0300

    Add files via upload

---
+++ exercicio1002/exercicio1002/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace exercicio1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, n, raio;
+            n = 3.14159;
+
+            Console.WriteLine("Digite um valor: ");
+            raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+           
+            area = n * Math.Pow(raio,2);
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+            
+        }
+    }
+}

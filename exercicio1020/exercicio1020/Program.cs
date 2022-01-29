bbf909f8679fb40de16ab0684f0dde974e554353
LuizFelipe13 <luiz_felipesbc@hotmail.com>
2022-01-29 16:33:50 -0300

    New exercises

---
+++ exercicio1020/exercicio1020/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercicio1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, ano, meses, dias, resto;
+
+            N = int.Parse(Console.ReadLine());
+
+           
+
+            ano = N / 365;
+            resto = N % 365;
+            meses = resto / 30;
+            dias = resto % 30;
+
+            Console.WriteLine(ano + " ano(s)");
+            Console.WriteLine(meses + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+        }
+    }
+}
